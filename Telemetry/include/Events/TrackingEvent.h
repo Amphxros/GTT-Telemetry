f017063bf0a73a0c41c84850e8baeb343a6cf123
Amparo Rubio Bellón <ampxrubio@gmail.com>
2022-04-05 18:38:51 +0200

    Reorganizacion del proyecto

---
+++ Telemetry/include/Events/TrackingEvent.h
@@ -0,0 +1,22 @@
+
+#pragma once
+
+#include <ctime>
+#include <string>
+
+class TrackingEvent
+{
+protected:
+    /* data */
+public:
+    TrackingEvent(/* args */);
+    ~TrackingEvent();
+};
+
+TrackingEvent::TrackingEvent(/* args */)
+{
+}
+
+TrackingEvent::~TrackingEvent()
+{
+}
