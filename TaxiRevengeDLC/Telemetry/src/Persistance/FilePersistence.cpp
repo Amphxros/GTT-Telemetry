b8697ca42e9ae9090b6211d22d2649c1fa328122
Amparo Rubio Bellón <ampxrubio@gmail.com>
2022-04-26 23:53:04 +0200

    persistencia a medias
    no puedo más la verdad

--- TaxiRevengeDLC/Telemetry/src/Persistance/FilePersistence.cpp
+++ TaxiRevengeDLC/Telemetry/src/Persistance/FilePersistence.cpp
@@ -1,10 +1,12 @@
 #include "../../include/Persistance/FilePersistence.h"
-#include "../../include/TrackingEvent.h"
+#include "../../include/Events/TrackingEvent.h"
+#include "../../include/Serialization/ISerializer.h"
 
 #include <chrono>
 #include <ctime>
 #include <fstream>
 #include <iostream>
+#include "../../include/Tracker.h"
 
 
 FilePersistence::FilePersistence(uint64_t time):
@@ -33,7 +35,35 @@ void FilePersistence::send(TrackingEvent* nEvent) {
 }
 
 void FilePersistence::flush() {
+    if (mEvents_.empty()) {
+        return;
+    }
+    std::ofstream mFile_;
 
+    try {
+        mEventMutex_.lock();
+        for (auto it = 0; it < mEvents_.size(); ++it) {
+            {
+                auto& ev = mEvents_.front();
+                mEvents_.pop();
+
+                mEventMutex_.unlock();
+                //add the event
+
+                mEventMutex_.lock();
+
+            }
+            mEventMutex_.unlock();
+
+            if (mFileName_.empty()) {
+                mFileName_ = "data/" + TrackerManager()->getIDSession() + mSerializer_->getExtension();
+            }
+
+        }
+    }
+    catch (std::exception& e) {
+
+    }
 }
 
 void FilePersistence::run()
