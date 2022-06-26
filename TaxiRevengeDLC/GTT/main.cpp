d2b8eec8f38024d6b06c12d9dc6523ee2bffe87e
dagil02 <dagil02@ucm.es>
2022-06-26 14:09:09 +0200

    tracker añadido

--- TaxiRevengeDLC/GTT/main.cpp
+++ TaxiRevengeDLC/GTT/main.cpp
@@ -11,14 +11,25 @@
 
 #include "Game.h"
 
+//Telemetry
+#include "../Telemetry/include/Tracker.h"
+
 using namespace std;
 
 typedef unsigned int uint;
 
 int main(int argc, char* argv[]) {
+	//Inicializacion 
+	Tracker* tracker = Tracker::getInstance();
+	tracker->init("id"); //Aqui hay que hacer que se genere un nuevo id
+
+
 	Game::getInstance()->initInstance(); //initInstance() only once
 	Game::getInstance()->run();
 	Game::destroyInstance();
+
+	//cierre del tracker
+	tracker->end();
 
 	return 0;
 }
